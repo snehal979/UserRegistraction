696fc57ff6bc0fe69d16ca80f06aa3ca3f58d4bf
Snehal979 <bansodsnehal535@gmail.com>
2022-12-13 12:21:14 +0530

    (Snehal)Add:validation for last name of person

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -5,10 +5,15 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("welcome to User Registration");
-            Validation validation = new Validation();
+            Validation Code = new Validation();
             Console.WriteLine("enter the first name");//"Snehal"
             string firstName = Console.ReadLine();
-            Console.WriteLine(validation.TestName(firstName));
+            Console.WriteLine(Code.TestName(firstName));
+            Console.WriteLine("--------------");
+            //uc2
+            Console.WriteLine("enter the last name");//"Bansod"
+            string lastName = Console.ReadLine();
+            Console.WriteLine(Code.TestName(lastName));
         }
     }
 }
