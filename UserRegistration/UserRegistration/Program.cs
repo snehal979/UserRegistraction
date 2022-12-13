f2e9a8b8329939b971e67153ddc8e959c4c0f1d9
Snehal979 <bansodsnehal535@gmail.com>
2022-12-13 12:31:24 +0530

    (Snehal)Add: validation of email all

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -1,19 +1,42 @@
-﻿namespace UserRegistration
+﻿using System;
+
+namespace UserRegistration
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("welcome to User Registration");
+            Console.WriteLine("hint 1. name(first/last) 2.email ");
+           
             Validation Code = new Validation();
-            Console.WriteLine("enter the first name");//"Snehal"
-            string firstName = Console.ReadLine();
-            Console.WriteLine(Code.TestName(firstName));
-            Console.WriteLine("--------------");
-            //uc2
-            Console.WriteLine("enter the last name");//"Bansod"
-            string lastName = Console.ReadLine();
-            Console.WriteLine(Code.TestName(lastName));
-        }
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("enter the first name");//"Snehal"
+                    string firstName = Console.ReadLine();
+                    Console.WriteLine(Code.TestName(firstName));
+                    Console.WriteLine("--------------");
+                    Console.WriteLine("enter the last name");//"Bansod"
+                    string lastName = Console.ReadLine();
+                    Console.WriteLine(Code.TestName(lastName));
+                    break;
+
+                case 2:
+                    Console.WriteLine("enter the Email");//"abc@xyzhg.com";
+                    string Email = Console.ReadLine();
+                    Console.WriteLine(Code.TestEmail(Email));
+                    /*1. abc@yahoo.com, -valid
+                   • 2. abc-100@yahoo.com,
+                   • 3. abc.100@yahoo.com
+                   • 2. abc111@abc.com,
+                   • 4. abc-100@abc.net,
+                   • 5. abc.100@abc.com.au
+                   • 6. abc@1.com, - Invalid
+                   • 7. abc@gmail.com.com
+                   • 8. abc+100@gmail.com */
+                    break;
+            }
     }
 }
--- UserRegistration/UserRegistration/Validation.cs
+++ UserRegistration/UserRegistration/Validation.cs
@@ -13,12 +13,25 @@ namespace UserRegistration
         /// Uc1 Test for First Name
         /// </summary>
         const string Name = "^[a-zA-Z]{3,}";
+        const string Email = "^[a-zA-Z0-9]+[.+-_]{0,1}[a-zA-Z0-9]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}";
         public string TestName(string input)
         {
             if (Regex.IsMatch(input, Name))
                 return "First/last name matches";
             else
                 return "First/last name regex failed";
+        }
+        /// <summary>
+        /// UC 3
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string TestEmail(string input)
+        {
+            if (Regex.IsMatch(input, Email))
+                return "Email  matches";
+            else
+                return "Email regex failed";
         }
     }
 }
