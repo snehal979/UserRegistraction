4eaa4240f0056fe23c88aa59951a5e74f36a4917
Snehal979 <bansodsnehal535@gmail.com>
2022-12-13 12:10:18 +0530

    (Snehal)Add:Validation Main Welcome Message

---
+++ UserRegistration/UserRegistration/Validation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Validation
+    {
+        /// <summary>
+        /// Uc1 Test for First Name
+        /// </summary>
+        const string Name = "^[a-zA-Z]{3,}";
+        public static string TestName(string input)
+        {
+            if (Regex.IsMatch(input, Name))
+                return "First/last name matches";
+            else
+                return "First/last name regex failed";
+        }
+    }
+}
